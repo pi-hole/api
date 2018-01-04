28d7b8881000cdc8b89ed6fa56c40db19f10d83a
Mcat12 <newtoncat12@yahoo.com>
2018-01-04 12:38:17 -0500

    Wrap decoding into an FtlConnection struct
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/ftl.rs
+++ src/ftl.rs
@@ -2,27 +2,45 @@ use std::os::unix::net::UnixStream;
 use std::error::Error;
 use std::io::prelude::*;
 use std::io::BufReader;
+use rmp::decode;
 
 const SOCKET_LOCATION: &'static str = "/var/run/pihole/FTL.sock";
 
-pub fn connect(command: &str) -> BufReader<UnixStream> {
+pub struct FtlConnection(BufReader<UnixStream>);
+
+pub fn connect(command: &str) -> FtlConnection {
     let mut stream = UnixStream::connect(SOCKET_LOCATION).unwrap();
     stream.write_all(format!(">{}\n", command).as_bytes()).unwrap();
 
-    BufReader::new(stream)
+    FtlConnection(BufReader::new(stream))
 }
 
-pub fn expect_eom<T: Read>(stream: &mut T) -> Result<(), String> {
-    let mut buffer: [u8; 1] = [0];
+impl FtlConnection {
+    pub fn expect_eom(&mut self) -> Result<(), String> {
+        let mut buffer: [u8; 1] = [0];
 
-    match stream.read_exact(&mut buffer) {
-        Ok(_) => (),
-        Err(e) => return Err(e.description().to_string())
+        match self.0.read_exact(&mut buffer) {
+            Ok(_) => (),
+            Err(e) => return Err(e.description().to_string())
+        }
+
+        if buffer[0] != 0xc1 {
+            return Err(format!("Expected EOM (0xc1), got {:2x}", buffer[0]));
+        }
+
+        Ok(())
     }
 
-    if buffer[0] != 0xc1 {
-        return Err(format!("Expected EOM (0xc1), got {:2x}", buffer[0]));
+    pub fn read_i32(&mut self) -> Result<i32, decode::ValueReadError> {
+        decode::read_i32(&mut self.0)
     }
 
-    Ok(())
+    pub fn read_f32(&mut self) -> Result<f32, decode::ValueReadError> {
+        decode::read_f32(&mut self.0)
+    }
+
+    pub fn read_u8(&mut self) -> Result<u8, decode::ValueReadError> {
+        decode::read_u8(&mut self.0)
+    }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,21 @@ extern crate rmp;
 mod util;
 mod ftl;
 
-use rmp::decode;
-
 #[get("/stats/summary")]
 fn summary() -> util::Reply {
-    let mut stream = ftl::connect("stats");
+    let mut con = ftl::connect("stats");
 
-    let domains_blocked = decode::read_i32(&mut stream).unwrap();
-    let total_queries = decode::read_i32(&mut stream).unwrap();
-    let blocked_queries = decode::read_i32(&mut stream).unwrap();
-    let percent_blocked = decode::read_f32(&mut stream).unwrap();
-    let unique_domains = decode::read_i32(&mut stream).unwrap();
-    let forwarded_queries = decode::read_i32(&mut stream).unwrap();
-    let cached_queries = decode::read_i32(&mut stream).unwrap();
-    let total_clients = decode::read_i32(&mut stream).unwrap();
-    let unique_clients = decode::read_i32(&mut stream).unwrap();
-    let status = decode::read_u8(&mut stream).unwrap();
-    ftl::expect_eom(&mut stream).unwrap();
+    let domains_blocked = con.read_i32().unwrap();
+    let total_queries = con.read_i32().unwrap();
+    let blocked_queries = con.read_i32().unwrap();
+    let percent_blocked = con.read_f32().unwrap();
+    let unique_domains = con.read_i32().unwrap();
+    let forwarded_queries = con.read_i32().unwrap();
+    let cached_queries = con.read_i32().unwrap();
+    let total_clients = con.read_i32().unwrap();
+    let unique_clients = con.read_i32().unwrap();
+    let status = con.read_u8().unwrap();
+    con.expect_eom().unwrap();
 
     util::reply_data(json!({
         "domains_blocked": domains_blocked,
