7445f829e1da8a916456b2c6bfb740d0c57cfdd9
Mcat12 <newtoncat12@yahoo.com>
2018-01-09 12:14:55 -0500

    Add license and add project metadata to Cargo.toml
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2018 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  API
+*  Program Main
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */
+
 #![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
--- src/web.rs
+++ src/web.rs
@@ -1,3 +1,13 @@
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2018 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  API
+*  Web Interface Endpoints
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */
+
 use rocket::response::NamedFile;
 use std::path::{Path, PathBuf};
 
