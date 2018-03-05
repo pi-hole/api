424bc3f46aff898ab04d01d30ee138b0b3707836
Mcat12 <newtoncat12@yahoo.com>
2018-03-04 21:24:12 -0500

    Add tests for get_list
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/dns/get_list.rs
+++ src/dns/get_list.rs
@@ -30,3 +30,87 @@ pub fn get_blacklist(config: State<Config>) -> util::Reply {
 pub fn get_wildlist(config: State<Config>) -> util::Reply {
     get_list(List::Wildlist, &config)
 }
+
+#[cfg(test)]
+mod test {
+    use testing::test_endpoint;
+    use config::PiholeFile;
+    use std::collections::HashMap;
+    use rocket::http::Method;
+
+    #[test]
+    fn test_get_whitelist() {
+        let whitelist = ["example.com", "example.net"].join("\n");
+        let setup_vars = "IPV4_ADDRESS=10.1.1.1";
+
+        let mut data = HashMap::new();
+        data.insert(PiholeFile::Whitelist, whitelist.into_bytes());
+        data.insert(PiholeFile::SetupVars, setup_vars.into());
+
+        test_endpoint(
+            Method::Get,
+            "/admin/api/dns/whitelist",
+            HashMap::default(),
+            data,
+            json!({
+                "data": [
+                    "example.com",
+                    "example.net"
+                ],
+                "errors": []
+            })
+        );
+    }
+
+
+    #[test]
+    fn test_get_blacklist() {
+        let blacklist = ["example.com", "example.net"].join("\n");
+        let setup_vars = "IPV4_ADDRESS=10.1.1.1";
+
+        let mut data = HashMap::new();
+        data.insert(PiholeFile::Blacklist, blacklist.into_bytes());
+        data.insert(PiholeFile::SetupVars, setup_vars.into());
+
+        test_endpoint(
+            Method::Get,
+            "/admin/api/dns/blacklist",
+            HashMap::default(),
+            data,
+            json!({
+                "data": [
+                    "example.com",
+                    "example.net"
+                ],
+                "errors": []
+            })
+        );
+    }
+
+    #[test]
+    fn test_get_wildlist() {
+        let wildlist = [
+            "address=/example.com/10.1.1.1",
+            "address=/example.net/10.1.1.1"
+        ].join("\n");
+        let setup_vars = "IPV4_ADDRESS=10.1.1.1";
+
+        let mut data = HashMap::new();
+        data.insert(PiholeFile::Wildlist, wildlist.into_bytes());
+        data.insert(PiholeFile::SetupVars, setup_vars.into());
+
+        test_endpoint(
+            Method::Get,
+            "/admin/api/dns/wildlist",
+            HashMap::default(),
+            data,
+            json!({
+                "data": [
+                    "example.com",
+                    "example.net"
+                ],
+                "errors": []
+            })
+        );
+    }
+}
