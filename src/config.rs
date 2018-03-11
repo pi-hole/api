41830590bb82f9d70823134d649eb57ac09523e9
Mcat12 <newtoncat12@yahoo.com>
2018-03-11 09:17:17 -0700

    Add missing license header
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,13 @@
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2018 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  API
+*  API Configuration
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */
+
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::os::unix::fs::OpenOptionsExt;
