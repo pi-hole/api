89cf7d937c9fa2f3339647d9caf2ca7d9d5bbb7d
Rob Gill <rrobgill@protonmail.com>
2018-06-22 12:26:51 +1000

    Requested changes
    Signed-off-by: Rob Gill <rrobgill@protonmail.com>

--- src/networkinfo.rs
+++ src/networkinfo.rs
@@ -3,70 +3,32 @@
 *  Network-wide ad blocking via your own hardware.
 *
 *  API
-*  Local network information
+*  Local hostname
 *
 *  This file is copyright under the latest version of the EUPL.
 *  Please see LICENSE file for your rights under this license. */
 
-extern crate hostname;
-
+use rocket::State;
 use setup_vars;
 use util;
-use config::{Env, Config};
-
-const CONFIG_LOCATION: &'static str = "/etc/pihole/API.toml";
+use config::{Env};
+use hostname;
 
 /// Get Pi-hole host name (not contained in setupvars.conf)
-fn gethost() -> String {
-    match hostname::get_hostname() {
-        None => {
-            return "unknown".to_owned();
-        }
-        Some(h) => {
-            return h;
-        }
-    }   
-}
-
-/// Get an entry from setupvars.conf, handle errors or nulls
-fn getsetupentry(searchentry: String) -> String {
-    let config = Config::parse(CONFIG_LOCATION).unwrap();
-    let env = Env::Production(config);
-    match setup_vars::read_setup_vars(&searchentry, &env) {
-        Ok(i) => {
-            match i {
-                Some(j) => { return j; }
-                None => { return "".to_owned(); }
-            }
-        }
-        Err(_) => {
-            return "error".to_owned();
-        }
-    }
-}
-
-/// Get Pi-hole IPv4 address
-fn getipv4() -> String {
-    getsetupentry("IPV4_ADDRESS".to_owned())
-}
-
-/// Get Pi-hole IPv6 address
-fn getipv6() -> String {
-    getsetupentry("IPV6_ADDRESS".to_owned())
-}
-
-/// Get Pi-hole hostname
-fn getinterface() -> String {
-    getsetupentry("PIHOLE_INTERFACE".to_owned())
+fn get_hostname() -> String {
+    hostname::get_hostname()
+        .unwrap_or("unknown".to_owned())
+        .to_owned()
 }
 
 /// Get Pi-hole local network information
-#[get("/networkinfo")]
-pub fn networkinfo() -> util::Reply {
+#[get("/settings/network")]
+pub fn network(env: State<Env>) -> util::Reply {
+
     return util::reply_data(json!({
-        "netinterface": getinterface(),
-        "netipv4": getipv4(),
-        "netipv6": getipv6(),
-        "nethost": gethost()
+        "interface": setup_vars::read_setup_vars(&"PIHOLE_INTERFACE".to_owned(), &env)?,
+        "ipv4_address": setup_vars::read_setup_vars(&"IPV4_ADDRESS".to_owned(), &env)?,
+        "ipv6_address": setup_vars::read_setup_vars(&"IPV6_ADDRESS".to_owned(), &env)?,
+        "hostname": get_hostname()
     }));  
 }
