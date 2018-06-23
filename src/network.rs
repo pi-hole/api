1d8cf281e74bfd5247186208040df642064f9368
Rob Gill <rrobgill@protonmail.com>
2018-06-23 13:41:06 +1000

    Call get_hostname directly
    Signed-off-by: Rob Gill <rrobgill@protonmail.com>

--- src/network.rs
+++ src/network.rs
@@ -3,31 +3,24 @@
 *  Network-wide ad blocking via your own hardware.
 *
 *  API
-*  Local hostname
+*  Local network information
 *
 *  This file is copyright under the latest version of the EUPL.
 *  Please see LICENSE file for your rights under this license. */
 
 use rocket::State;
 use setup_vars::read_setup_vars;
-use util;
+use util::{Reply, reply_data};
 use config::{Env};
-use hostname;
-
-/// Get Pi-hole host name
-fn get_hostname() -> String {
-    hostname::get_hostname()
-        .unwrap_or("unknown".to_owned())
-}
+use hostname::get_hostname;
 
 /// Get Pi-hole local network information
 #[get("/settings/network")]
-pub fn network(env: State<Env>) -> util::Reply {
-    return util::reply_data(json!({
+pub fn network(env: State<Env>) -> Reply {
+    return reply_data(json!({
         "interface": read_setup_vars(&"PIHOLE_INTERFACE", &env)?.unwrap_or_default(),
         "ipv4_address": read_setup_vars(&"IPV4_ADDRESS", &env)?.unwrap_or_default(),
         "ipv6_address": read_setup_vars(&"IPV6_ADDRESS", &env)?.unwrap_or_default(),
-        "hostname": get_hostname()
+        "hostname": get_hostname().unwrap_or("unknown".to_owned())
     }));  
 }
-
