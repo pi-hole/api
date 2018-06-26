758adef15b5faa6ba7076d227b94dfe37532d107
Rob Gill <rrobgill@protonmail.com>
2018-06-26 13:30:14 +1000

    Don't return mask
    Split IPv4, return address only
    
    Signed-off-by: Rob Gill <rrobgill@protonmail.com>

--- src/network.rs
+++ src/network.rs
@@ -13,14 +13,17 @@ use setup_vars::read_setup_vars;
 use util::{Reply, reply_data};
 use config::{Env};
 use hostname::get_hostname;
+use auth::User;
 
 /// Get Pi-hole local network information
 #[get("/settings/network")]
-pub fn network(env: State<Env>) -> Reply {
+pub fn network(env: State<Env>, _auth: User) -> Reply {
+    let ipv4_full = read_setup_vars("IPV4_ADDRESS", &env)?.unwrap_or_default();
+    let ipv4_address: Vec<&str> = ipv4_full.split("/").collect();
     return reply_data(json!({
         "interface": read_setup_vars("PIHOLE_INTERFACE", &env)?.unwrap_or_default(),
-        "ipv4_address": read_setup_vars("IPV4_ADDRESS", &env)?.unwrap_or_default(),
+        "ipv4_address": &ipv4_address[0],
         "ipv6_address": read_setup_vars("IPV6_ADDRESS", &env)?.unwrap_or_default(),
         "hostname": get_hostname().unwrap_or("unknown".to_owned())
-    }));  
+    }));
 }
