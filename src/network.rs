12ef5471bee95d2ff5b0b28c93ae2ab0cec6c262
Rob Gill <rrobgill@protonmail.com>
2018-06-27 08:30:09 +1000

    Split ipv6
    Signed-off-by: Rob Gill <rrobgill@protonmail.com>

--- src/network.rs
+++ src/network.rs
@@ -20,10 +20,12 @@ use auth::User;
 pub fn network(env: State<Env>, _auth: User) -> Reply {
     let ipv4_full = read_setup_vars("IPV4_ADDRESS", &env)?.unwrap_or_default();
     let ipv4_address: Vec<&str> = ipv4_full.split("/").collect();
+    let ipv6_full = read_setup_vars("IPV6_ADDRESS", &env)?.unwrap_or_default();
+    let ipv6_address: Vec<&str> = ipv6_full.split("/").collect();
     return reply_data(json!({
         "interface": read_setup_vars("PIHOLE_INTERFACE", &env)?.unwrap_or_default(),
         "ipv4_address": &ipv4_address[0],
-        "ipv6_address": read_setup_vars("IPV6_ADDRESS", &env)?.unwrap_or_default(),
+        "ipv6_address": &ipv6_address[0],
         "hostname": get_hostname().unwrap_or("unknown".to_owned())
     }));
 }
