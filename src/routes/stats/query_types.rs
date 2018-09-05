e58f9fd15fc159f53cb5caa4013917cf7c689f51
Mcat12 <newtoncat12@yahoo.com>
2018-09-05 11:36:40 -0400

    Use shared memory in `/stats/query_types` and update output format
    It outputs the same thing as `/stats/summary`'s `total_queries` field.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/routes/stats/query_types.rs
+++ src/routes/stats/query_types.rs
@@ -9,84 +9,79 @@
 // Please see LICENSE file for your rights under this license.
 
 use auth::User;
-use ftl::FtlConnectionType;
+use ftl::{FtlMemory, FtlQueryType};
 use rocket::State;
-use util::{reply_data, reply_error, ErrorKind, Reply};
+use util::{reply_data, Reply};
 
 /// Get the query types
 #[get("/stats/query_types")]
-pub fn query_types(_auth: User, ftl: State<FtlConnectionType>) -> Reply {
-    let mut con = ftl.connect("querytypes")?;
+pub fn query_types(_auth: User, ftl_memory: State<FtlMemory>) -> Reply {
+    let counters = ftl_memory.counters()?;
 
-    // Create a 4KiB string buffer
-    let mut str_buffer = [0u8; 4096];
-    let mut query_types = Vec::new();
-
-    loop {
-        // Read in the name, unless we are at the end of the list
-        let name = match con.read_str(&mut str_buffer) {
-            Ok(name) => name.to_string(),
-            Err(e) => {
-                // Check if we received the EO
-                // Check if we received the EOM
-                if e.kind() == ErrorKind::FtlEomError {
-                    break;
-                }
-
-                // Unknown read error
-                return reply_error(e);
-            }
-        };
-
-        let percent = con.read_f32()?;
-
-        query_types.push(QueryType { name, percent });
-    }
-
-    reply_data(query_types)
-}
-
-#[derive(Serialize)]
-struct QueryType {
-    name: String,
-    percent: f32
+    reply_data(json!([
+        {
+            "name": "A",
+            "count": counters.query_type(FtlQueryType::A)
+        },
+        {
+            "name": "AAAA",
+            "count": counters.query_type(FtlQueryType::AAAA)
+        },
+        {
+            "name": "ANY",
+            "count": counters.query_type(FtlQueryType::ANY)
+        },
+        {
+            "name": "SRV",
+            "count": counters.query_type(FtlQueryType::SRV)
+        },
+        {
+            "name": "SOA",
+            "count": counters.query_type(FtlQueryType::SOA)
+        },
+        {
+            "name": "PTR",
+            "count": counters.query_type(FtlQueryType::PTR)
+        },
+        {
+            "name": "TXT",
+            "count": counters.query_type(FtlQueryType::TXT)
+        }
+    ]))
 }
 
 #[cfg(test)]
 mod test {
-    use rmp::encode;
-    use testing::{write_eom, TestBuilder};
+    use ftl::{FtlCounters, FtlMemory};
+    use std::collections::HashMap;
+    use testing::TestBuilder;
 
+    fn test_data() -> FtlMemory {
+        FtlMemory::Test {
+            counters: FtlCounters {
+                query_type_counters: [2, 2, 1, 1, 1, 2, 1],
+                total_queries: 10,
+                ..FtlCounters::default()
+            },
+            strings: HashMap::new(),
+            clients: Vec::new()
+        }
+    }
+
+    /// Simple test to validate output
     #[test]
-    fn test_query_types() {
-        let mut data = Vec::new();
-        encode::write_str(&mut data, "A (IPv4)").unwrap();
-        encode::write_f32(&mut data, 0.1).unwrap();
-        encode::write_str(&mut data, "AAAA (IPv6)").unwrap();
-        encode::write_f32(&mut data, 0.2).unwrap();
-        encode::write_str(&mut data, "ANY").unwrap();
-        encode::write_f32(&mut data, 0.3).unwrap();
-        encode::write_str(&mut data, "SRV").unwrap();
-        encode::write_f32(&mut data, 0.4).unwrap();
-        encode::write_str(&mut data, "SOA").unwrap();
-        encode::write_f32(&mut data, 0.5).unwrap();
-        encode::write_str(&mut data, "PTR").unwrap();
-        encode::write_f32(&mut data, 0.6).unwrap();
-        encode::write_str(&mut data, "TXT").unwrap();
-        encode::write_f32(&mut data, 0.7).unwrap();
-        write_eom(&mut data);
-
+    fn query_types() {
         TestBuilder::new()
             .endpoint("/admin/api/stats/query_types")
-            .ftl("querytypes", data)
+            .ftl_memory(test_data())
             .expect_json(json!([
-                { "name": "A (IPv4)", "percent": 0.10000000149011612 },
-                { "name": "AAAA (IPv6)", "percent": 0.20000000298023225 },
-                { "name": "ANY", "percent": 0.30000001192092898 },
-                { "name": "SRV", "percent": 0.4000000059604645 },
-                { "name": "SOA", "percent": 0.5 },
-                { "name": "PTR", "percent": 0.6000000238418579 },
-                { "name": "TXT", "percent": 0.699999988079071 }
+                { "name": "A", "count": 2 },
+                { "name": "AAAA", "count": 2 },
+                { "name": "ANY", "count": 1 },
+                { "name": "SRV", "count": 1 },
+                { "name": "SOA", "count": 1 },
+                { "name": "PTR", "count": 2 },
+                { "name": "TXT", "count": 1 }
             ]))
             .test();
     }
