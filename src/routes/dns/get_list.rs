9e0145b8629d8825e4691e5ce97e8fb28e327ffb
Mcat12 <newtoncat12@yahoo.com>
2019-05-18 00:11:35 -0700

    Add back get_list tests
    They now properly mock the service.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/routes/dns/get_list.rs
+++ src/routes/dns/get_list.rs
@@ -30,3 +30,54 @@ pub fn get_blacklist(service: ListServiceGuard) -> Reply {
 pub fn get_regexlist(service: ListServiceGuard) -> Reply {
     reply_result(service.get(List::Regex))
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        lists::{List, ListServiceMock},
+        testing::TestBuilder
+    };
+    use mock_it::verify;
+
+    /// Test that the domains are returned correctly
+    fn get_test(list: List, endpoint: &str, domains: Vec<String>) {
+        let service = ListServiceMock::new();
+
+        service.get.given(list).will_return(Ok(domains.clone()));
+
+        TestBuilder::new()
+            .endpoint(endpoint)
+            .mock_service(service.clone())
+            .expect_json(json!(domains))
+            .test();
+
+        assert!(verify(service.get.was_called_with(list)));
+    }
+
+    #[test]
+    fn test_get_whitelist() {
+        get_test(
+            List::White,
+            "/admin/api/dns/whitelist",
+            vec!["example.com".to_owned(), "example.net".to_owned()]
+        );
+    }
+
+    #[test]
+    fn test_get_blacklist() {
+        get_test(
+            List::Black,
+            "/admin/api/dns/blacklist",
+            vec!["example.com".to_owned(), "example.net".to_owned()]
+        );
+    }
+
+    #[test]
+    fn test_get_regexlist() {
+        get_test(
+            List::Regex,
+            "/admin/api/dns/regexlist",
+            vec!["^.*example.com$".to_owned(), "example.net".to_owned()]
+        );
+    }
+}
