92358cb5b4902be380a06429855fd5f3edcc77e6
Mcat12 <newtoncat12@yahoo.com>
2019-05-18 00:04:34 -0700

    Add back delete_list tests
    They now properly mock the service.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/routes/dns/delete_list.rs
+++ src/routes/dns/delete_list.rs
@@ -34,3 +34,61 @@ pub fn delete_regexlist(_auth: User, list_service: ListServiceGuard, domain: Str
     list_service.remove(List::Regex, &domain)?;
     reply_success()
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        lists::{List, ListServiceMock},
+        testing::TestBuilder
+    };
+    use mock_it::verify;
+    use rocket::http::Method;
+
+    /// Test that a successful delete returns success
+    fn delete_test(list: List, endpoint: &str, domain: &str) {
+        let service = ListServiceMock::new();
+
+        service
+            .remove
+            .given((list, domain.to_owned()))
+            .will_return(Ok(()));
+
+        TestBuilder::new()
+            .endpoint(endpoint)
+            .method(Method::Delete)
+            .mock_service(service.clone())
+            .expect_json(json!({ "status": "success" }))
+            .test();
+
+        assert!(verify(
+            service.remove.was_called_with((list, domain.to_owned()))
+        ));
+    }
+
+    #[test]
+    fn test_delete_whitelist() {
+        delete_test(
+            List::White,
+            "/admin/api/dns/whitelist/example.com",
+            "example.com"
+        );
+    }
+
+    #[test]
+    fn test_delete_blacklist() {
+        delete_test(
+            List::Black,
+            "/admin/api/dns/blacklist/example.com",
+            "example.com"
+        );
+    }
+
+    #[test]
+    fn test_delete_regexlist() {
+        delete_test(
+            List::Regex,
+            "/admin/api/dns/regexlist/%5E.%2Aexample.com%24",
+            "^.*example.com$"
+        );
+    }
+}
